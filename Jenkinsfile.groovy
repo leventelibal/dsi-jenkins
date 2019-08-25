a182043466a9b05955bf6f58ecd75cb868d61bcc
leventelibal <leventelibal@gmail.com>
2019-08-25 16:02:17 -0400

    add mv and version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,6 +18,9 @@ pipeline{
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
                     sh "unzip terraform_0.12.7_linux_amd64.zip"
+                    sh "sudo mv terraform /bin"
+                    sh "terraform version"
+                    
                 }
             }
         }
