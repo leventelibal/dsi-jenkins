0c7aca86f3475338e8fbfbfe0270db07289f3498
leventelibal <leventelibal@gmail.com>
2019-08-25 16:15:39 -0400

    add sudo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,12 +19,12 @@ pipeline{
                         def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             "
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
                             sh "unzip -o terrafsh "terraform versionorm_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version
                         }
                     }
